14e847664ae78f87d9ef57fc4bf12145cf4270f5
Shashank Pathak <shashankhalo7@gmail.com>
2023-07-29 00:33:00 +0530

    Update utils.py

--- Session 11/utils.py
+++ Session 11/utils.py
@@ -88,3 +88,69 @@ def plot_loss_and_accuracy(train_losses, train_acc, test_losses, test_acc):
     axs[0, 1].set_title("Test Loss")
     axs[1, 1].plot(test_acc)
     axs[1, 1].set_title("Test Accuracy")
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+def get_incorrect_images(model,test_loader,n=10):
+  incorrect_images = []
+  predicted_labels = []
+  correct_labels = []
+  for data, target in test_loader:
+    data, target = data.to(device), target.to(device)
+    output = model(data)
+    pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    incorrect_items = pred.ne(target.view_as(pred))
+    incorrect_indices = incorrect_items.view(-1).nonzero().view(-1)
+    predicted_labels.extend([item.item() for item in pred[incorrect_indices[:n-len(incorrect_images)]]])
+    correct_labels.extend([item.item() for item in target.view_as(pred)[incorrect_indices[:n-len(incorrect_images)]]])
+    incorrect_images.extend([item for item in data[incorrect_indices[:n-len(incorrect_images)]]])
+    if len(incorrect_images)==n:
+      break
+  return incorrect_images,predicted_labels,correct_labels
+
+def imshow(img):
+  img = img / 2 + 0.5     # Unnormalize
+  npimg = img
+  npimg = np.clip(npimg, 0, 1)  # Add this line to clip the values
+  return np.transpose(npimg, (1, 2, 0))  # Convert from Tensor image
+
+fig, axes = plt.subplots(2, 5, figsize=(16, 8))
+
+incorrect_images,predicted_labels,correct_labels = get_incorrect_images(model,test_loader,n=10)
+
+for i, image_tensor in enumerate(incorrect_images):
+    ax = axes[i // 5, i % 5]  # Get the location of the subplot
+    image = image_tensor.cpu().numpy()
+    ax.imshow(imshow(image))  # Display the image
+    ax.set_title(f"Predicted {class_names[predicted_labels[i]]}, Actual {class_names[correct_labels[i]]}")  # Set the title as the index
+
+plt.tight_layout()  # To provide sufficient spacing between subplots
+plt.show()
+
+
+
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+
+target_layers = [model.layer4[-1]]
+cam = GradCAM(model=model, target_layers=target_layers, use_cuda=True)
+
+fig, axes = plt.subplots(2, 5, figsize=(16, 8))
+
+for i, image_tensor in enumerate(incorrect_images):
+    ax = axes[i // 5, i % 5]  # Get the location of the subplot
+    image = image_tensor.cpu().numpy()
+    grayscale_cam = cam(input_tensor=image_tensor.reshape(1,3,32,32), targets=[ClassifierOutputTarget(predicted_labels[i])],aug_smooth=True,eigen_smooth=True)
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(imshow(image), grayscale_cam, use_rgb=True,image_weight=0.6)
+    #ax.imshow(np.transpose(imshow(visualization), (2, 0, 1)))  # Display the image
+    ax.imshow(visualization,interpolation='bilinear')
+    ax.set_title(f"Predicted {class_names[predicted_labels[i]]}, Actual {class_names[correct_labels[i]]}")  # Set the title as the index
+
+plt.tight_layout()  # To provide sufficient spacing between subplots
+plt.show()
