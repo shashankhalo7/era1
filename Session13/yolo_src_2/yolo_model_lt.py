c95b5609a188ce9f41a52f5c2192b55b228ae076
shashankhalo7 <shashankhalo7@gmail.com>
2023-09-21 16:20:21 +0530

    Fix yolo src

--- Session13/yolo_src_2/yolo_model_lt.py
+++ Session13/yolo_src_2/yolo_model_lt.py
@@ -125,7 +125,16 @@ class YOLOv3(pl.LightningModule):
         
         self.config = config
         self.scaled_anchors = (torch.tensor(self.config.ANCHORS)* torch.tensor(self.config.S).unsqueeze(1).unsqueeze(1).repeat(1, 3, 2))
-        self.loss_fn = YoloLoss()
+        self.mse = nn.MSELoss()
+        self.bce = nn.BCEWithLogitsLoss()
+        self.entropy = nn.CrossEntropyLoss()
+        self.sigmoid = nn.Sigmoid()
+
+        # Constants signifying how much to pay for each respective part of the loss
+        self.lambda_class = 1
+        self.lambda_noobj = 10
+        self.lambda_obj = 1
+        self.lambda_box = 10
         self.train_loader = train_loader
         self.test_loader = test_loader
         
@@ -211,13 +220,13 @@ class YOLOv3(pl.LightningModule):
     def on_epoch_start(self):
          self.plot_couple_examples(self, self.test_loader, 0.6, 0.5, self.scaled_anchors)
         
-    def training_epoch_end(self):
+    def on_train_epoch_end(self):
         current_epoch = self.current_epoch
         if self.config.SAVE_MODEL:
             self.save_checkpoint(self, self.optimizers(), filename=f"checkpoint_{current_epoch}.pth.tar")
         self.check_class_accuracy(self, self.train_loader, threshold=self.config.CONF_THRESHOLD,logger=self.log)
         
-    def validation_epoch_end(self):
+    def on_valid_epoch_end(self):
         current_epoch = self.current_epoch
         if current_epoch % 10 == 0 or current_epoch in (1,2,3):
             self.check_class_accuracy(self, self.test_loader, threshold=self.config.CONF_THRESHOLD,logger=self.log)
@@ -795,4 +804,57 @@ class YOLOv3(pl.LightningModule):
             boxes[..., [0, 2]] = boxes[..., [0, 2]].clip(0, shape[1])  # x1, x2
             boxes[..., [1, 3]] = boxes[..., [1, 3]].clip(0, shape[0])  # y1, y2
             
-        
+    
+    def loss_fn(self, predictions, target, anchors):
+        # Check where obj and noobj (we ignore if target == -1)
+        obj = target[..., 0] == 1  # in paper this is Iobj_i
+        noobj = target[..., 0] == 0  # in paper this is Inoobj_i
+
+        # ======================= #
+        #   FOR NO OBJECT LOSS    #
+        # ======================= #
+
+        no_object_loss = self.bce(
+            (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
+        )
+
+        # ==================== #
+        #   FOR OBJECT LOSS    #
+        # ==================== #
+
+        anchors = anchors.reshape(1, 3, 1, 1, 2)
+        box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors], dim=-1)
+        ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
+        object_loss = self.mse(self.sigmoid(predictions[..., 0:1][obj]), ious * target[..., 0:1][obj])
+
+        # ======================== #
+        #   FOR BOX COORDINATES    #
+        # ======================== #
+
+        predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])  # x,y coordinates
+        target[..., 3:5] = torch.log(
+            (1e-16 + target[..., 3:5] / anchors)
+        )  # width, height coordinates
+        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+
+        # ================== #
+        #   FOR CLASS LOSS   #
+        # ================== #
+
+        class_loss = self.entropy(
+            (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
+        )
+
+        #print("__________________________________")
+        #print(self.lambda_box * box_loss)
+        #print(self.lambda_obj * object_loss)
+        #print(self.lambda_noobj * no_object_loss)
+        #print(self.lambda_class * class_loss)
+        #print("\n")
+
+        return (
+            self.lambda_box * box_loss
+            + self.lambda_obj * object_loss
+            + self.lambda_noobj * no_object_loss
+            + self.lambda_class * class_loss
+        )
